9440d80cc98813c6bb44204d36fabe78b8f53d63
sanjeev-iitr-uk <sanjeev@hometown.in>
2019-12-30 11:23:26 +0530

    Updates

---
+++ precache-manifest.ad66e2edba0311c3cf41a78dc0d1aee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d31f156b4ac3d7677a51da4b3a2dcd",
+    "url": "/demo-page/index.html"
+  },
+  {
+    "revision": "79bb003efe5127309e9d",
+    "url": "/demo-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "490d6a4052e1f1dea913",
+    "url": "/demo-page/static/js/2.4b0df06a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo-page/static/js/2.4b0df06a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79bb003efe5127309e9d",
+    "url": "/demo-page/static/js/main.7a2eac6b.chunk.js"
+  },
+  {
+    "revision": "e7555b8e435a1c9f9c50",
+    "url": "/demo-page/static/js/runtime-main.25d48e7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
